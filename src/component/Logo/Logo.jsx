b0c2b7642f15a2d77609c822d1e5ffa202fea13e
shahneouj <shahneouj@gmail.com>
2025-12-10 14:11:01 +0600

    navbar and logo added

---
+++ src/component/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BsHouseUpFill } from "react-icons/bs";
+const Logo = () => {
+  return (
+    <>
+      <div className="logo ">
+        <BsHouseUpFill className="text-primary" />
+        <h2 className="text-primary">StyleDecor</h2>
+      </div>
+    </>
+  );
+};
+
+export default Logo;
